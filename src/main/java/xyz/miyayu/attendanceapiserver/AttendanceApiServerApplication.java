b0c336b384ff24a3f2feb310f4c21a4ccf3a566e
Miyayu <yuchan2215@gmail.com>
2023-12-06 15:55:27 +0900

    First Commit

---
+++ src/main/java/xyz/miyayu/attendanceapiserver/AttendanceApiServerApplication.java
@@ -0,0 +1,13 @@
+package xyz.miyayu.attendanceapiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendanceApiServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AttendanceApiServerApplication.class, args);
+    }
+
+}
