02328ac9242b2466b1ccdcb0b1e930ef9270e039
Miyayu <yuchan2215@gmail.com>
2024-01-24 12:34:03 +0900

    add: StudentのJsonキーを変更

--- src/main/java/xyz/miyayu/attendanceapiserver/Entity/StudentEntity.java
+++ src/main/java/xyz/miyayu/attendanceapiserver/Entity/StudentEntity.java
@@ -1,5 +1,6 @@
 package xyz.miyayu.attendanceapiserver.Entity;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -11,18 +12,23 @@ import lombok.Setter;
 public class StudentEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonProperty("id")
     @Column(name = "auto_id")
     private int autoId;
 
+    @JsonProperty("studentId")
     @Column(name = "student_id", length = 100)
     private String studentId;
 
+    @JsonProperty("name")
     @Column(name = "name", length = 100)
     private String name;
 
+    @JsonProperty("departmentId")
     @Column(name = "department_id")
     private int departmentId;
 
+    @JsonProperty("icId")
     @Column(name = "ic_id", length = 100)
     private String icId = null;
 }
